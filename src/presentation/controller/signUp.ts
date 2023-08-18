18c8af3cbd931bf70cb9b816a277aee91a2b7477
Allain-Prost <allain.prost@dcx.ufpb.br>
2023-08-18 13:42:37 -0300

    refactor: ensure SingUpController.handle implement correct interfaces

--- src/presentation/controller/signUp.ts
+++ src/presentation/controller/signUp.ts
@@ -1,5 +1,7 @@
+import type { HttResponse, HttpRequest } from '../protocols/http'
+
 export default class SignUpController {
-  handle (httpResquest: any): any {
+  handle (httpResquest: HttpRequest): HttResponse {
     if (!httpResquest.body.name) {
       return {
         statusCode: 400,
@@ -11,6 +13,10 @@ export default class SignUpController {
         statusCode: 400,
         body: new Error('Missing param: email')
       }
+    }
+    return {
+      statusCode: 200,
+      body: 'Success'
     }
   }
 }
