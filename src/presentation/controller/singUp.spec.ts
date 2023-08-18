855778169853b29558561cb5eabd6e267877df42
Allain-Prost <allain.prost@dcx.ufpb.br>
2023-08-18 14:25:17 -0300

    test: ensure SIgnUpController return 400 if no passwordConfirmation is provided

--- src/presentation/controller/singUp.spec.ts
+++ src/presentation/controller/singUp.spec.ts
@@ -43,4 +43,18 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
+
+  test('Should return 400 if no passwordConfirmation is provided', () => {
+    const sut = new SignUpController()
+    const httpResquest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@gmail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpResquest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
+  })
 })
