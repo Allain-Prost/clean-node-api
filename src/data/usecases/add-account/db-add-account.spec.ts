db9f5642806992f7926ecd3f7a851b463caa832e
Allain-Prost <allain.prost@dcx.ufpb.br>
2023-08-23 23:31:19 -0300

    test: ensure DbAddAccount throws if Encrypert throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -36,4 +36,20 @@ describe('DbAddAccount Usecase', () => {
     await sut.add(accountData)
     expect(encryptSpy).toHaveBeenCalledWith('valid_password')
   })
+
+  // Repassar a exceção que a gente receber do encrypter
+  // Foi mocado uma dependência do SUT, no qual essa dependência retorna um exceção
+  // E quando eu chamar o SUT eu espero que o proprio SUT retorne um exceção
+  // Esse teste visa garantir que não vamos tratar um erro dentro do nosso método add lá no DbAddAccount
+  test('Should throw if Encrypter throws', async () => {
+    const { sut, encrypterStub } = makeSut()
+    jest.spyOn(encrypterStub, 'encrypt').mockReturnValueOnce(new Promise((resolve, reject) => { reject(new Error()) }))
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const promise = sut.add(accountData)
+    await expect(promise).rejects.toThrow()
+  })
 })
